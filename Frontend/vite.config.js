b87ded7d2d064a74f7fe39e40679306918253e74
aswinbabu01 <151752328+aswinbabu01@users.noreply.github.com>
2024-07-21 17:25:27 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3457',
+        target: 'https://artvista-backend-fnh0.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
